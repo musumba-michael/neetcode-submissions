03f6ff92ec9ab289929fe54e6e2aa48d3d40d310
Gerald Michael Musumba <103953618+musumba-michael@users.noreply.github.com>
2026-08-10 07:54:12 +0300

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        // seen hashmap
+        // set and check if num in set
+        // brute force with nested loops
+
+        // using et
+        let set = new Set<number>()
+        for (const num of nums){
+            if (set.has(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
