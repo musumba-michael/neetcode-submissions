30d4e18a34ca022036ddf7e53b44783d17373ccb
Gerald Michael Musumba <103953618+musumba-michael@users.noreply.github.com>
2026-09-22 09:42:35 +0300

    Add: duplicate-integer - submission-15

---
+++ Data Structures & Algorithms/duplicate-integer/submission-15.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        for i in 0..nums.len(){
+            for j in (i + 1)..nums.len(){
+                if nums[i] == nums[j]{
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
