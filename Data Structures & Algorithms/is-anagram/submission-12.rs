a1112b8eab126f029c17a6948592e9c03a316b7d
Gerald Michael Musumba <103953618+musumba-michael@users.noreply.github.com>
2026-09-22 10:25:27 +0300

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len(){
+            return false;
+        }
+
+    let mut count: [i32; 26] = Default::default();   
+                 
+        for (a, b) in s.bytes().zip(t.bytes()){
+            count[(a - b'a') as usize] += 1;
+            count[(b - b'a') as usize] -= 1;
+        }
+
+        count.iter().all(|&elem| elem == 0)
+    }
+}
